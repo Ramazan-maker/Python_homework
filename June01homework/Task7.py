503139f717933e6e8eb91880fca9306e4c9c11be
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-08 15:29:49 +0600

    Проверено дз от 01.06
    Комментарии к коду

--- June01homework/Task7.py
+++ June01homework/Task7.py
@@ -3,7 +3,10 @@ def get_total_seconds(hours, minutes, seconds):
         total_seconds = hours * 3600 + minutes * 60 + seconds
         return total_seconds
     except TypeError:
+        # VN: функция, задача которой - вычисление, не должна заниматься вводом-выводом
+        # лучше здесь написать return None. Тем более, что ниже в программе у вас есть проверка на это
         print("Некорректные данные! Пожалуйста, введите целые числа для часов, минут и секунд.")
+
 hours = int(input("Введите часы: "))
 minutes = int(input("Введите минуты: "))
 seconds = int(input("Введите секунды: "))
